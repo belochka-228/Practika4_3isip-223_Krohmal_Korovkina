78b1828b076d6038893b2bc3d8a27ab38380b64c
belochka-228 <sofikrohmal@gmail.com>
2026-03-02 11:35:36 +0300

    Добавьте файлы проекта.

---
+++ Практическая 4_3исип-223_Крохмаль_Коровкина/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Практическая_4_3исип_223_Крохмаль_Коровкина
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
